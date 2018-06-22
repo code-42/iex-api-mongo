1c28a1c7054b2b12ae4927de3301e3cbf61a5e9e
Edward Dupre <edwd42@gmail.com>
2018-06-22 13:45:54 -0400

    initial commit

---
+++ src/main/java/net/ed/api/IexApiSpringApplication.java
@@ -0,0 +1,12 @@
+package net.ed.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IexApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IexApiSpringApplication.class, args);
+	}
+}
