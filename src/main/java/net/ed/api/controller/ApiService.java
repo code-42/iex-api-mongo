fa24f327da2ed3e99331bd6dab815a46c89bf7ae
Edward Dupre <edwd42@gmail.com>
2018-06-24 21:14:42 -0400

    add @Autowired ApiRepository field to ApiService class

--- src/main/java/net/ed/api/controller/ApiService.java
+++ src/main/java/net/ed/api/controller/ApiService.java
@@ -1,10 +1,12 @@
 package net.ed.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ApiService {
 	
-
+	@Autowired
+	private ApiRepository apiRepository;
 
 }
