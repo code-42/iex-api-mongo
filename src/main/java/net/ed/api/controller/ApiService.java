ab9c22a7c6555d6aa9fc46a5c5420d024d6ae8c6
Edward Dupre <edwd42@gmail.com>
2018-06-24 21:17:31 -0400

    add ApiRepository.save() method to ApiService through @Autowired field

--- src/main/java/net/ed/api/controller/ApiService.java
+++ src/main/java/net/ed/api/controller/ApiService.java
@@ -3,10 +3,16 @@ package net.ed.api.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import net.ed.api.entity.Chart;
+
 @Service
 public class ApiService {
 	
 	@Autowired
 	private ApiRepository apiRepository;
+	
+	public void addChart(Chart chart) {
+		apiRepository.save(chart);
+	}
 
 }
