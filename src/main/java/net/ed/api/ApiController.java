db5ca39a203183103642884c8a64549273cc5899
Edward Dupre <edwd42@gmail.com>
2019-01-08 14:20:47 -0500

    add symbol field to json returned from iex api

--- src/main/java/net/ed/api/ApiController.java
+++ src/main/java/net/ed/api/ApiController.java
@@ -1,119 +1,71 @@
 package net.ed.api;
 
+// https://www.mkyong.com/webservices/jax-rs/restfull-java-client-with-java-net-url/
+
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.boot.CommandLineRunner;
-//import java.util.Arrays;
-//import java.util.List;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-//import org.springframework.scheduling.annotation.EnableScheduling;
-//import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-import net.ed.api.ApiRepository;
 
-import net.ed.api.Stocks;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import net.ed.api.JsonDecoder;
+import org.json.JSONArray;
 
-//@EnableScheduling
-@SpringBootApplication
-@RestController
 @Component
-public class ApiController { // implements CommandLineRunner {
+public class ApiController implements CommandLineRunner {
 	
-//	@Autowired
-	private ApiRepository apiRepository;
-	
-//	public ApiController(ApiRepository apiRepository) {
-//		this.apiRepository = apiRepository;
-//	}
-	
-	@PostMapping
-	public void save(Stocks stock) {
-		this.apiRepository.insert(stock);
-	}
-	
-	String[] symbols = {"AAPL","INTC","AMZN","AMD","NFLX","MU","BABA","FB","BAC","MSFT"};
-	
-	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.build();
-	}
-	
-    public void buildJson(String[] stock) {
-    	String keys[] = {"symbol","market_date","open","high","low","close","volume"};
-//    	String values[] = {"AMZN", "2019-01-04","1530.0","1594.0","1518.31","1575.39","9182575"};
-    	String values[] = stock;
-    	System.out.println(stock.toString());
-    	    	String stockJson = "{";
-    	for (int i = 0; i < keys.length; i++) {
-	    	stockJson += "'" + keys[i] + "':'" + values[i + 1] + "'";
-	    	if(i < keys.length - 1) { stockJson += ",";}
-    	}
-    	stockJson += "}";
-    	System.out.println(stockJson);
-	};
-	
-//	@Bean
-//	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
-//	public void run() throws Exception {
-	public void run() throws Exception {
+	public void run(String... args) throws Exception {
 		
-		System.out.println("Helllllooooo");
-
 		LocalDate yesterday = LocalDate.now().minusDays(1);
 		DateTimeFormatter dateFmt = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-		System.out.println("61. yesterday == " + yesterday.format(dateFmt));
+//		System.out.println("28. yesterday == " + yesterday.format(dateFmt));
 		
-//		List<Stocks> stocksArr = Arrays.asList();
-		
-//		return args -> {
-//			for(String symbol : symbols) {
-//				System.out.println(symbol);
-//				String[] stocks = restTemplate.getForObject(
-//					"https://api.iextrading.com/1.0/stock/" + symbol + "/chart", String[].class);
-////
-//				for(String stock : stocks) {
-//					stock.setSymbol(symbol);
-//					System.out.println(stock.toString());
-//					if(stock.date().equals("2018-12-27")) {
-//					if(stock.getDate().equals(yesterday.format(dateFmt))) {
-//						System.out.println(stock.toString());
-//						buildJson(stock);
-//						stocksArr.add(stock);
-					}
-////					this.apiRepository.saveAll(stocksArr);
-//				}
-////				this.apiRepository.saveAll(stocksArr);
-//
-//				List<Stocks> stocksArr = Arrays.asList(stocks);
-//				for(Stocks stock : stocksArr) {
-//					System.out.println(stock);
-//				}
-//			};
-////			this.apiRepository.saveAll(stocksArr);
-//
-//			System.exit(0);
-//		};
-//		System.exit(0);
-//		return null;
-//	}
-//
-//	@Override
-//	public void run(String... args) throws Exception {
-//		// TODO Auto-generated method stub
-//	}
+		String[] symbols = {"AAPL","INTC","AMZN","AMD","NFLX","MU","BABA","FB","BAC","MSFT"};
 
-}
+		
+		try {
+			for(String symbol : symbols) {
+				URL url = new URL("https://api.iextrading.com/1.0/stock/" + symbol + "/chart");
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				conn.setRequestMethod("GET");
+				conn.setRequestProperty("Accept", "application/json");
+				
+				if (conn.getResponseCode() != 200) {
+					throw new RuntimeException("Failed : HTTP error code : "
+							+ conn.getResponseCode());
+				}
+			
+				BufferedReader br = new BufferedReader(new InputStreamReader(
+					(conn.getInputStream())));
+				
+				String output; // looks like array of Json objects but is String
+				while ((output = br.readLine()) != null) {
+					JsonDecoder.jsonDecoder(symbol, output);
+				}
+				conn.disconnect();
+			}  // end for
 	
-
+		  } catch (MalformedURLException e) {
+		
+			e.printStackTrace();
+		
+		  } catch (IOException e) {
+		
+			e.printStackTrace();
+		
+		  } catch (Exception e) {
+				
+			e.printStackTrace();
+		
+		  }
+		System.exit(0);
+	}
+}
--- src/main/java/net/ed/api/JsonDecoder.java
+++ src/main/java/net/ed/api/JsonDecoder.java
@@ -5,6 +5,8 @@ package net.ed.api;
 import java.io.IOException;
 import java.io.StringReader;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import com.google.gson.Gson; 
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
@@ -17,78 +19,76 @@ import com.google.gson.stream.JsonToken;
 class JsonDecoder {
 
    public static void jsonDecoder(String symbol, String jsonStr) {
-	   
-//	GsonBuilder builder = new GsonBuilder(); 
-//	builder.setPrettyPrinting(); 
-//	Gson gson = builder.create();
-//	Gson jsonG = new Gson();
-//	
-	JsonParser parser = new JsonParser();
-	
-	try {
-        Object obj = parser.parse(jsonStr);
-        JsonArray array = (JsonArray)obj;
 
-        System.out.println("The last element of array");
-        String jsonString = (array.get(array.size()-1)).toString();
-        System.out.println(jsonString);
-        System.out.println("\n");
+	   	JsonParser parser = new JsonParser();
 		
-        JsonReader jsonReader = new JsonReader(new StringReader(jsonString));
-		
-        // http://tutorials.jenkov.com/java-json/gson-jsonreader.html
-		while(jsonReader.hasNext()) {
-	        JsonToken nextToken = jsonReader.peek();
-	        System.out.println(nextToken);
-	        
-
-	        if(JsonToken.BEGIN_OBJECT.equals(nextToken)){
-
-	            jsonReader.beginObject();
-
-	        } else if(JsonToken.NAME.equals(nextToken)){
-
-	            String name  =  jsonReader.nextName();
-	            System.out.println(name);
-
-	        } else if(JsonToken.STRING.equals(nextToken)){
-
-	            String value =  jsonReader.nextString();
-	            System.out.println(value);
-	            
-	        } else if(JsonToken.NUMBER.equals(nextToken)){
-	        	try {
-	        		long value =  jsonReader.nextLong();
-	        		System.out.println(value);
-	        		
-		        } catch (Exception e) {
-//		        	    e.printStackTrace();
-	        	}
-	        	
-	        } else if(JsonToken.NUMBER.equals(nextToken)){
-	        	try {
-	        		double value =  jsonReader.nextDouble();
-	        		System.out.println(value);
-	        		
-		        } catch (Exception e) {
-//		        	    e.printStackTrace();
-	        	}
-        	}
-	        
-			}
-		
-			if(!jsonReader.hasNext()) {
-				jsonReader.endObject();
-			}
+		try {
 			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			System.out.println("inside catch");
-			e.printStackTrace();
+//			System.out.println("The last element of array");
+	        JsonArray array = (JsonArray) parser.parse(jsonStr);
+	        String jsonString = (array.get(array.size()-1)).toString();	        
+	    	JsonObject jsonObject = new JsonParser().parse(jsonString).getAsJsonObject();
+	    	jsonObject.addProperty("symbol", symbol);
+//	    	System.out.println();
+	    	
+	    	String doc = jsonObject.toString();
+	    	System.out.println(doc);
+//	    	System.out.println();
 
-	   } catch (Exception e) {
-		    e.printStackTrace();
-		}
+	    	
+	        // http://tutorials.jenkov.com/java-json/gson-jsonreader.html
+//	    	JsonReader jsonReader = new JsonReader(new StringReader(jsonString));
+//			while(jsonReader.hasNext()) {
+//		        JsonToken nextToken = jsonReader.peek();
+//		        System.out.println(nextToken);
+//		        
+//	
+//		        if(JsonToken.BEGIN_OBJECT.equals(nextToken)){
+//	
+//		            jsonReader.beginObject();
+//	
+//		        } else if(JsonToken.NAME.equals(nextToken)){
+//	
+//		            String name  =  jsonReader.nextName();
+//		            System.out.println(name);
+//	
+//		        } else if(JsonToken.STRING.equals(nextToken)){
+//	
+//		            String value =  jsonReader.nextString();
+//		            System.out.println(value);
+//		            
+//		        } else if(JsonToken.NUMBER.equals(nextToken)){
+//		        	try {
+//		        		long value =  jsonReader.nextLong();
+//		        		System.out.println(value);
+//		        		
+//			        } catch (Exception e) {
+////			        	    e.printStackTrace();
+//		        	}
+//		        	
+//		        } else if(JsonToken.NUMBER.equals(nextToken)){
+//		        	try {
+//		        		double value =  jsonReader.nextDouble();
+//		        		System.out.println(value);
+//		        		
+//			        } catch (Exception e) {
+////			        	    e.printStackTrace();
+//		        	}
+//	        	}
+//		        
+//				}
+//			
+//				if(!jsonReader.hasNext()) {
+//					jsonReader.endObject();
+//				}
+//			} catch (IOException e) {
+//				// TODO Auto-generated catch block
+//				System.out.println("inside catch");
+//				e.printStackTrace();
 	
-   }
+		   	} catch (Exception e) {
+			    e.printStackTrace();
+			}
+		
+	   }
 }
